cd9f3de17852d14cd32785e56500c1dfb684e906
raquelbiondi <biondiraquel@gmail.com>
2023-04-25 23:52:48 -0300

    retoques

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,12 @@
+//cipher.encode(offset, string)
+//offset é numero de posiçoes que user deseja mover pra DIREITA no alfabeto
+//string é a mensagem (texto) que user deseja CIFRAR
+//cipher.decode(offset, string)
+//offset é o número de posições que queremos mover para a ESQUERDA no alfabeto 
+//string é a mensagem (texto) que queremos DECIFRAR
+
 const cipher = {
   encode: function encode(key, encodeWord) {
    
+
 export default cipher;
