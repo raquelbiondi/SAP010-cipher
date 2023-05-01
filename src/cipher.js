aa5fbc8c1886bd06b6d24f08384a5c82ea23126d
raquelbiondi <biondiraquel@gmail.com>
2023-05-01 18:28:35 -0300

    final

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,45 @@
-//cipher.encode(offset, string)
-//offset é numero de posiçoes que user deseja mover pra DIREITA no alfabeto
-//string é a mensagem (texto) que user deseja CIFRAR
-//cipher.decode(offset, string)
-//offset é o número de posições que queremos mover para a ESQUERDA no alfabeto 
-//string é a mensagem (texto) que queremos DECIFRAR
-
 const cipher = {
-  encode: function encode(key, encodeWord) {
-   
+  encode: function (chave, texto){
+    if (texto === "" || chave === "") {
+      alert("Por favor, preencha todos os campos");
+    }
+    
+    let textoCodificado = "";
+    texto = texto.toUpperCase();
+
+    for (let i = 0; i < texto.length; i++){
+      const position = texto.charCodeAt(i);
+      
+      if (position >= 65 && position <= 90){
+        textoCodificado += String.fromCharCode (((position - 65 + chave) % 26) + 65);
+      }
+      if (position >= 32 && position <= 64){
+        textoCodificado += texto.charAt(i);
+      }
+    }
+    return textoCodificado;
+  },
+
+  decode: function (chave,texto){
+    if (chave === "" || texto === "") {
+      alert("Prencha todos os campos");
+    }
+    
+    let textoDescodificado = "";
+    texto = texto.toUpperCase();
+
+    for (let i = 0; i < texto.length; i++){
+      const position = texto.charCodeAt(i);
+      
+      if (position >= 65 && position <= 90){
+        textoDescodificado += String.fromCharCode (((position + 65 - chave) % 26) + 65);
+      }
+      if (position >= 32 && position <= 64){
+        textoDescodificado += texto.charAt(i);
+      }
+    }
+    return textoDescodificado;
+  },  
+};
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 import cipher from "./cipher.js"
 
-document.getElementById("encodebutton").addEventListener("click", function () {
-  const encodeWord = document.getElementById("encodetext").value;
+console.log(cipher);
+
+const offset = document.getElementById("secret-key");
+const box1 = document.getElementById("entertext");
+const crip = document.getElementById("cript");
+const box2 = document.getElementById("textexit");
+const descrip = document.getElementById("descript");
+
+crip.addEventListener("click", acaoCode);
+descrip.addEventListener("click", acaoDecode);
+
+function acaoCode() {
+  box1.value = cipher.encode(parseInt(offset.value), box1.value);
+}
+
+function acaoDecode() {
+  box2.value = cipher.decode(parseInt(offset.value), box1.value);
+}
